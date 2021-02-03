0e99b19359172322a8b1acca41886ae218dddc52
JunYoung-choi1 <cjyoung10@naver.com>
2021-02-03 16:41:11 +0900

    product list,detail

--- src/main/java/com/example/demo/product/ProductController.java
+++ src/main/java/com/example/demo/product/ProductController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.board.Board;
@@ -27,13 +28,25 @@ public class ProductController {
 	public ModelAndView List() {
 		System.out.println("ProductController.List()");
 		ArrayList<Product> list = (ArrayList<Product>) service.getProductAll();
-		
+		System.out.println(list.toString());
 		ModelAndView mav = new ModelAndView("product/list");
 		mav.addObject("list", list);
 		return mav;
 	}
 		
+	@RequestMapping("/product/detail")
+	public ModelAndView detail(@RequestParam(value = "num")int num) {
+		ModelAndView mav = new ModelAndView("product/detail");
+		Product p = service.getProductByNum(num);
+		System.out.println("ProductController.detail()");
+		mav.addObject("p", p);
 		
+		return mav;
+		
+		
+	}
+	
+	
 	}
 	
 	
---
+++ src/main/webapp/WEB-INF/views/product/detail.jsp
@@ -0,0 +1,65 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>Insert title here</title>
+</head>
+<body>
+<h3>상품 상제 정보</h3>
+
+
+<table border="1" cellspacing="0">
+<tr>
+<th>번호</th>
+<td><input type="text" name="num" value="${p.num }">
+</td>
+</tr>
+
+<tr>
+<th>상품명</th>
+<td><input type="text" name="name" value="${p.name }">
+</td>
+</tr>
+
+<tr>
+<th>메이커</th>
+<td><input type="text" name="maker" value="${p.maker }">
+</td>
+</tr>
+
+<tr>
+<th>가격</th>
+<td><input type="text" name="price" value="${p.price }">
+</td>
+</tr>
+
+<tr>
+<th>원산지</th>
+<td><input type="text" name="origin" value="${p.origin }">
+</td>
+</tr>
+
+<tr>
+<th>재료</th>
+<td><input type="text" name="material" value="${p.material }">
+</td>
+</tr>
+
+<tr>
+<th>수량</th>
+<td><input type="text" name="quantity" value="${p.quantity }"></td>
+</tr>
+
+<tr>
+<td colspan="2">
+<input type="submit" value="수정">
+<input type="button" value="삭제" id="del">
+</td>
+</tr>
+
+</table>
+
+</body>
+</html>
